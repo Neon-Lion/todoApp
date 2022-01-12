577a30c02c5853688c1563e5abd37bd09cde8f2b
Neon-Lion <ivoadjemov@gmail.com>
2022-01-12 15:44:24 +0200

    change indention using spaces

--- src/App.js
+++ src/App.js
@@ -7,39 +7,39 @@ import { TodoList }   from "./components/TodoList";
 import { TodosCount } from "./components/TodosCount";
 
 const todos = [
-  {'id': 1, 'title': 'Learn HTML', 'completed': false},
-  {'id': 2, 'title': 'Learn CSS', 'completed': false},
-  {'id': 3, 'title': 'Learn JavaScript', 'completed': false}
+    {'id': 1, 'title': 'Learn HTML', 'completed': false},
+    {'id': 2, 'title': 'Learn CSS', 'completed': false},
+    {'id': 3, 'title': 'Learn JavaScript', 'completed': false}
 ];
 
 function App() {
-  return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                Learn React
+                </a>
+            </header>
+        </div>
 
     <div className="page">
-      <Header />
-      <main className="todo-app">
-        <AddTodo />
-        <TodoList />
-        <TodosCount />
-      </main>
+        <Header />
+        <main className="todo-app">
+            <AddTodo />
+            <TodoList />
+            <TodosCount />
+        </main>
     </div>
-  );
+    );
 }
 
 export {App, todos};
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const TodoItem = (props) => {
     return (
-      <li id={props.id}>
-          <span className={props.completed ? 'completed' : ''}>{props.title}</span>
-          <div className="action-icon complete"><i className="far fa-check-square"></i></div>
-          <div className="action-icon edit"><i className="fas fa-pencil-alt"></i></div>
-          <div className="action-icon remove"><i className="far fa-trash-alt"></i></div>
-      </li>
+        <li id={props.id}>
+            <span className={props.completed ? 'completed' : ''}>{props.title}</span>
+            <div className="action-icon complete"><i className="far fa-check-square"></i></div>
+            <div className="action-icon edit"><i className="fas fa-pencil-alt"></i></div>
+            <div className="action-icon remove"><i className="far fa-trash-alt"></i></div>
+        </li>
     );
 }
 
